ee4cf75909380314cc50b2fe7d895d4e6af56461
tranhuynhbang <tranhuynhbang95@gmail.com>
2022-08-09 19:16:48 +0700

    Build Multiple Browser_Lv1 complete

---
+++ testcases/com/techpanda/account/Level_04_Multiple_Browser.java
@@ -0,0 +1,134 @@
+package com.techpanda.account;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import pageObjects.user.HomePageObject;
+import pageObjects.user.LoginPageObject;
+import pageObjects.user.MyDashBoardPageObject;
+
+public class Level_04_Multiple_Browser extends BaseTest {
+	WebDriver driver;
+
+	HomePageObject homePage;
+	LoginPageObject loginPage;
+	MyDashBoardPageObject myDashboardPage;
+
+	@Parameters({"browser"})
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driver = getBrowserDriver(browserName);
+		
+		homePage = new HomePageObject(driver);
+	}
+
+	@Test
+	public void TC_01_LoginWithEmptyEmailAndPassword() {
+
+		homePage.clickTToMyAccountLink();
+		loginPage = new LoginPageObject(driver);
+
+		loginPage.inputToEmailAddressTextbox("");
+		loginPage.inputToPasswordTextbox("");
+		loginPage.clickToLoginButton();
+
+		assertEquals(loginPage.getEmailAddressEmptyErrorMessage(), "This is a required field.");
+		assertEquals(loginPage.getPasswordAddressEmptyErrorMessage(), "This is a required field.");
+
+	}
+
+	@Test
+	public void TC_02_LoginWithInvalidEmail() {
+
+		homePage.clickTToMyAccountLink();
+		loginPage = new LoginPageObject(driver);
+
+		loginPage.inputToEmailAddressTextbox("123@456.789");
+		loginPage.inputToPasswordTextbox("123456");
+		loginPage.clickToLoginButton();
+
+		assertEquals(loginPage.getEmailAddressInvalidErrorMessage(),
+				"Please enter a valid email address. For example johndoe@domain.com.");
+
+	}
+
+	@Test(description = "Email not exist in application")
+	public void TC_03_LoginWithIncorrectEmail() {
+
+		homePage.clickTToMyAccountLink();
+		loginPage = new LoginPageObject(driver);
+
+		loginPage.inputToEmailAddressTextbox("auto_test" + randomNumber() + "@live.com");
+		loginPage.inputToPasswordTextbox("123456");
+		loginPage.clickToLoginButton();
+
+		assertEquals(loginPage.getEmailAddressIncorrectErrorMessage(), "Invalid login or password.");
+
+	}
+
+	@Test(description = "Password less than 6 characters")
+	public void TC_04_LoginWithInvalidPassword() {
+
+		homePage.clickTToMyAccountLink();
+		loginPage = new LoginPageObject(driver);
+
+		loginPage.inputToEmailAddressTextbox("auto_test" + randomNumber() + "@live.com");
+		loginPage.inputToPasswordTextbox("123");
+		loginPage.clickToLoginButton();
+
+		assertEquals(loginPage.getPasswordInvalidErrorMessage(),
+				"Please enter 6 or more characters without leading or trailing spaces.");
+	}
+
+	@Test
+	public void TC_05_LoginWithIncorrectPassword() {
+
+		homePage.clickTToMyAccountLink();
+		loginPage = new LoginPageObject(driver);
+
+		loginPage.inputToEmailAddressTextbox("auto_test" + randomNumber() + "@live.com");
+		loginPage.inputToPasswordTextbox(randomNumber() + "");
+		loginPage.clickToLoginButton();
+
+		assertEquals(loginPage.getPasswordIncorectErrorMessage(), "Invalid login or password.");
+
+	}
+
+	@Test
+	public void TC_06_LoginWithValidEmailAndPassword() {
+
+		homePage.clickTToMyAccountLink();
+		loginPage = new LoginPageObject(driver);
+
+		loginPage.inputToEmailAddressTextbox("automationfc.vn@gmail.com");
+		loginPage.inputToPasswordTextbox("123123");
+		loginPage.clickToLoginButton();
+
+		myDashboardPage = new MyDashBoardPageObject(driver);
+		
+
+		assertTrue(myDashboardPage.getContactNameTextDisplay("Automation FC"));
+		
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	private int randomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(999999);
+	}
+
+}
